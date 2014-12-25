55f18413287c5b4a3e1fdb7963ac9bdc8263d8f2
MyCah <mycahp@gmail.com>
2014-12-25 00:41:07 -0500

    Add start to processing

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 # Index
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # Create our variables from our form
+        font = request.form['font']
+        fontcolor = request.form['fontcolor']
+        bgcolor = request.form['bgcolor']
+        quote = request.form['quote']
+        squotemarks = request.form['quotemarks']
+        
+        # A return to tell our template to display the image
+        display = True
+    else:
+        display = False
+    
     fonts = ('Arial', 'Courier New', 'Verdana', 'Times New Roman')
-    return render_template('index.html', fonts = fonts)
+    return render_template('index.html', fonts = fonts, display = display)
