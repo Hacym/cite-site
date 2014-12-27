44bd8b275686fc6fa2037bfb362f4e8682063020
MyCah <mycahp@gmail.com>
2014-12-27 01:26:17 -0500

    Put image creation in function
    Only allow 4 image sizes to make more aesthetically pleasing images

---
+++ app/functions.py
@@ -0,0 +1,58 @@
+# Create image function -- pass the entire request form object to it.
+def create_image(request):
+    from PIL import Image, ImageDraw, ImageFont
+    
+    if request['size'] == "small":
+        size = (300, 200)
+        attrposition = (10, 150)
+        quotemarkposition = (200, 125)
+        quotemarksize = 125
+        fontsize = 15
+    elif request['size'] == "medium":
+        size = (600, 400)
+        attrposition = (10, 350)
+        quotemarkposition = (400, 300)
+        quotemarksize = 250
+        fontsize = 30
+    elif request['size'] == "large":
+        size = (1200, 800)
+        attrposition = (10, 625)
+        quotemarkposition = (1000, 525)
+        quotemarksize = 500
+        fontsize = 60
+    elif request['size'] == "xlarge":
+        size = (2400, 1600)
+        attrposition = (10, 1425)
+        quotemarkposition = (1975, 1300)
+        quotemarksize = 1000
+        fontsize = 120
+        
+    # Make our font object
+    font = ImageFont.truetype('app/static/fonts/'+request['font']+'.ttf', fontsize)
+    
+    # Create our instance of the image class from Pillow
+    image = Image.new('RGB', size, (255, 255, 255, 0))
+    
+    # Create our draw object
+    draw = ImageDraw.Draw(image)
+    
+    # Positioned in the top left for now
+    position = (10, 10)
+    
+    # Draw the text
+    draw.text(position, request['quote'], font=font, fill=(0, 0, 0, 255))
+        
+    if request['attribution'] != "":
+        # Put the quote attribution on the image
+        draw.text(attrposition, "- " +request['attribution'], font = font, fill=(0, 0, 0, 255))
+    
+    if request['quotemarks'] == "True":
+        # Just a default position now, can make it do something more advanced later (should appear in bottom right hand corner)
+        font = ImageFont.truetype('app/static/fonts/'+request['font']+'.ttf', quotemarksize)
+        draw.text(quotemarkposition, '"', font=font, fill=(0, 0, 0, 255))
+    
+    # Bye bye draw instance
+    del draw
+    
+    # Save our image
+    image.save('app/static/quote.PNG')
--- app/routes.py
+++ app/routes.py
@@ -1,66 +1,18 @@
 from app import app
 from flask import render_template, request, url_for
-from PIL import Image, ImageDraw, ImageFont
 import os, base64
 
 # Index
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Create our variables from our form
-        font = ImageFont.truetype('app/static/fonts/'+request.form['font']+'.ttf', 15)
+        from functions import create_image
         
-        # Breaking out our colors into RGB to use in Pillow --
-        # this is done with Javascript on the template side
-        fontcolor_r = request.form['font-r']
-        fontcolor_g = request.form['font-g']
-        fontcolor_b = request.form['font-b']
+        # create_image function from functions.py, pass it the entire request form object.
+        # This function creates our image for us
+        create_image(request.form)
         
-        bgcolor_r = request.form['bg-r']
-        bgcolor_g = request.form['bg-g']
-        bgcolor_b = request.form['bg-b']
-        
-        quote = request.form['quote']
-        quotemarks = request.form['quotemarks']
-        
-        # Must make these an int because for some stupid reason a number input type comes in as a string.
-        height = int(request.form['height'])
-        width = int(request.form['width'])
-        
-        attribution = "- " + request.form['attribution']
-        
-        # Custom image height and width
-        size = (width, height)
-        image = Image.new('RGB', size, (255, 255, 255, 0)) # Create our instance of the image class from Pillow
-        
-        # Create our draw object
-        draw = ImageDraw.Draw(image)
-        
-        # Positioned in the top left for now
-        position = (10, 10)
-        
-        # Draw the text
-        draw.text(position, quote, font=font, fill=(0, 0, 0, 255))
-        
-        # Set our new position for the attribution
-        position = (10, height - 50)
-        
-        # Put the quote attribution on the image
-        draw.text(position, attribution, font = font, fill=(0, 0, 0, 255))
-        
-        if quotemarks == "True":
-            # Just a default position now, can make it do something more advanced later (should appear in bottom right hand corner)
-            position = (width - 75, height - 75)
-            font = ImageFont.truetype('app/static/fonts/'+request.form['font']+'.ttf', 125)
-            draw.text(position, '"', font=font, fill=(0, 0, 0, 255))
-        
-        # Bye bye draw instance
-        del draw
-        
-        # Save our image
-        image.save('app/static/quote.PNG')
-        
-        # Open it back up...
+        # We need to open the image back up so that it can be encoded
         imgfile = open('app/static/quote.PNG')
         
         # Encode it as base64 for use in the template (cache issue resolved)
@@ -76,6 +28,5 @@ def index():
     # Our list of fonts. 
     # These fonts have to be TTF!!
     fonts = [font.rstrip('.ttf') for font in os.listdir('app/static/fonts') if font.endswith('.ttf')]
-        
     
     return render_template('index.html', fonts = fonts, display = display, imagesrc = imagesrc)
