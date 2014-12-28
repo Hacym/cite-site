8dcd0e3daf02f9d97d7924eff3aaa85ffea26ff0
MyCah <mycahp@gmail.com>
2014-12-28 02:37:15 -0500

    Add text wrapping for quotes

--- app/functions.py
+++ app/functions.py
@@ -2,18 +2,17 @@
 def create_image(request):
     from PIL import Image, ImageDraw, ImageFont
     
-    
     # Set our font and image sizes
     if request['size'] == "small":
         size = (300, 200)
         attrposition = (10, 150)
-        quotemarkposition = (200, 125)
+        quotemarkposition = (250, 125)
         quotemarksize = 125
         fontsize = 15
     elif request['size'] == "medium":
         size = (600, 400)
         attrposition = (10, 350)
-        quotemarkposition = (400, 300)
+        quotemarkposition = (400, 275)
         quotemarksize = 250
         fontsize = 30
     elif request['size'] == "large":
@@ -25,10 +24,14 @@ def create_image(request):
     elif request['size'] == "xlarge":
         size = (2400, 1600)
         attrposition = (10, 1425)
-        quotemarkposition = (1975, 1300)
+        quotemarkposition = (1975, 1100)
         quotemarksize = 1000
         fontsize = 120
-        
+    
+    # Add new lines so that text doesn't overflow. Ugly for now.
+    import textwrap
+    quote = textwrap.wrap(request['quote'], 24)
+    
     # Make our font object for use with the quote and attribution text
     font = ImageFont.truetype('app/static/fonts/'+request['font']+'.ttf', fontsize)
     
@@ -39,10 +42,12 @@ def create_image(request):
     draw = ImageDraw.Draw(image)
     
     # Positioned in the top left for now
-    position = (10, 10)
+    position = [10, 10]
     
-    # Draw the text
-    draw.text(position, request['quote'], font=font, fill=(0, 0, 0, 255))
+    for line in quote:
+        # Draw the text
+        draw.text(position, line, font=font, fill=(0, 0, 0, 255))
+        position[1] += fontsize
         
     if request['attribution'] != "":
         # Put the quote attribution on the image
@@ -58,3 +63,5 @@ def create_image(request):
     
     # Save our image
     image.save('app/static/quote.PNG')
+    
+    return quote
